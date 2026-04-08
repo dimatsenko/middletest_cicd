0347f46b8c542e0dadfb3a2535ab3ed1c6c15e76
dmytro <dimatsenko@gmail.com>
2026-04-08 15:24:02 +0300

    add from_dict method to the task class

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ class Task:
             "created_at": self.created_at,
             "is_done": self.is_done
         }
+
+    @staticmethod
+    def from_dict(data):
+        return Task(data['id'], data['description'], data['priority'], data['created_at'], data['is_done'])
+
+    def __str__(self):
+        status = "[V]" if self.is_done else "[ ]"
+        return f"ID: {self.task_id} | {status} Пріоритет: {self.priority} | Створено: {self.created_at} | Опис: {self.description}"
