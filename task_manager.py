1018d29b6c448f06d2b927b8095e1b5b8c6d15d2
dmytro <dimatsenko@gmail.com>
2026-04-08 15:33:41 +0300

    add detele and add to task class

--- task_manager.py
+++ task_manager.py
@@ -21,3 +21,18 @@ class TaskManager:
     def save_tasks(self):
         with open(self.filename, 'w', encoding='utf-8') as f:
             json.dump([task.to_dict() for task in self.tasks], f, ensure_ascii=False, indent=4)
+
+            def add_task(self, description, priority):
+                new_id = max([t.task_id for t in self.tasks], default=0) + 1
+                new_task = Task(new_id, description, priority)
+                self.tasks.append(new_task)
+                self.save_tasks()
+                return new_id
+
+            def delete_task(self, task_id):
+                initial_count = len(self.tasks)
+                self.tasks = [t for t in self.tasks if t.task_id != task_id]
+                if len(self.tasks) < initial_count:
+                    self.save_tasks()
+                    return True
+                return False
